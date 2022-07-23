7ecf31e72dcdc8bd6d361c09b7e77d02800aa09f
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-07-23 19:14:34 +0530

    Added : with Servlet to create Dynamic Web Pages

---
+++ src/main/java/com/LoginServlet.java
@@ -0,0 +1,44 @@
+package com;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+                @WebInitParam(name = "user", value = "Ajit"),
+                @WebInitParam(name = "password", value = "Ajit@123")
+        }
+)
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get request parameter for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        //get servlet config init params
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        String regexName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+
+        String regexPassword = "^[A-Z]{1}[a-z0-9!@#$%^&*]{7,}$";
+        if (userID.equals(user) && userID.matches(regexName) && password.equals(pwd) && userID.matches(regexPassword)) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+        } else {
+            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either username or password is wrong.</font>");
+            requestDispatcher.include(request, response);
+        }
+    }
+}
